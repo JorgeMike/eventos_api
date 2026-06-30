e9137c76858e4f8a9613341dffdfc5405df5ceee
gmartinez <gabymquezada@gmail.com>
2026-06-30 00:55:08 +0000

    Se agrega la conexión a base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({isGlobal: true}),
+  TypeOrmModule.forRootAsync({
+    useFactory: (config: ConfigService) => ({
+      type: 'mariadb',
+      host: config.get<string>('DB_HOST'),
+      port: parseInt(config.get<string>('DB_PORT')!),
+      username: config.get<string>('DB_USERNAME'),
+      password: config.get<string>('DB_PASSWORD'),
+      database: config.get<string>('DB_NAME'),
+      synchronize: true,
+    }),    
+    inject: [ConfigService],
+  }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = app.get(ConfigService);
+  console.log(config.get<string>('DB_HOST'));
+  console.log(config.get<string>('DB_PORT'));
+  console.log(config.get<string>('DB_USERNAME'));
+  console.log(config.get<string>('DB_PASSWORD'));
+  console.log(config.get<string>('DB_NAME')); 
+  await app.listen(config.get<string>('PORT') ?? 3000);
 }
 bootstrap();
