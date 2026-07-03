e80e54a04c143f10e2270fde46bc4c111c6b39ac
gmartinez <gabymquezada@gmail.com>
2026-07-03 00:05:39 +0000

    Se modifican relaciones de tablas

--- src/eventos/entity/eventos.entity.ts
+++ src/eventos/entity/eventos.entity.ts
@@ -1,37 +1,47 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { EventoSeccion } from './eventoseccion.entity';
 
 @Entity()
 export class Eventos {
-    @PrimaryGeneratedColumn ()
-    id_evento!: number
+  @PrimaryGeneratedColumn()
+  id_evento!: number;
 
-    @Column ()
-    nombre!: string
-    
-    @Column ({nullable : true})
-    descripcion?: string
-    
-    @Column ()
-    editable!: boolean
-    
-    @Column ({nullable : true})
-    fecha_maxima_registro?: number
-    
-    @Column ()
-    fecha_inicio!: number
-    
-    @Column ()
-    fecha_fin!: number
-    
-    @Column ({nullable : true})
-    cupo_maximo?: number
-    
-    @Column ({nullable : true})
-    banner?: string
-    
-    @Column ({nullable : true})
-    mensaje_confirmacion?: string
-    
-    @Column ({nullable : true})
-    exclusivo?: boolean
+  @Column()
+  nombre!: string;
+
+  @Column({ nullable: true })
+  descripcion?: string;
+
+  @Column()
+  editable!: boolean;
+
+  @Column({ nullable: true })
+  fecha_maxima_registro?: number;
+
+  @Column()
+  fecha_inicio!: number;
+
+  @Column()
+  fecha_fin!: number;
+
+  @Column({ nullable: true })
+  cupo_maximo?: number;
+
+  @Column({ nullable: true })
+  banner?: string;
+
+  @Column({ nullable: true })
+  mensaje_confirmacion?: string;
+
+  @Column({ nullable: true })
+  exclusivo?: boolean;
+
+  @OneToMany(() => EventoSeccion, (eventoSeccion) => eventoSeccion.evento)
+  eventoSecciones!: EventoSeccion[];
 }
--- src/eventos/entity/eventoseccion.entity.ts
+++ src/eventos/entity/eventoseccion.entity.ts
@@ -1,20 +1,26 @@
-import { Column, Entity, PrimaryGeneratedColumn, OneToOne, JoinColumn} from "typeorm";
-import { Eventos } from "./eventos.entity";
-import { Secciones } from "src/secciones/entity/secciones.entity";
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  JoinColumn,
+  ManyToOne,
+} from 'typeorm';
+import { Eventos } from './eventos.entity';
+import { Secciones } from 'src/secciones/entity/secciones.entity';
 
-@Entity({name: "evento_seccion"})
+@Entity({ name: 'evento_seccion' })
 export class EventoSeccion {
-    @PrimaryGeneratedColumn ()
-    id_evento_seccion!: number
+  @PrimaryGeneratedColumn()
+  id_evento_seccion!: number;
 
-    @Column ()
-    posicion!: string
+  @Column()
+  posicion!: string;
 
-    @OneToOne(() => Eventos) 
-       @JoinColumn({name: "id_evento"})
-       evento!: Eventos   
+  @ManyToOne(() => Eventos)
+  @JoinColumn({ name: 'id_evento' })
+  evento!: Eventos;
 
-     @OneToOne(() => Secciones) 
-       @JoinColumn({name: "id_seccion"})
-       seccion!: Secciones  
+  @ManyToOne(() => Secciones)
+  @JoinColumn({ name: 'id_seccion' })
+  seccion!: Secciones;
 }
